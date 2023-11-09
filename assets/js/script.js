f4838ea46a451ccbf46f9a802097c0bc468c2a30
Blake Skor <blake.skor@outlook.com>
2023-11-09 02:53:13 -0500

    Api call and parse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
 const openWeatherApiKey = "ed3486fa717dade80795e0620a2c18af";
 
-let city_name = "Hartford";
-let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city_name}&&appid=${openWeatherApiKey}`;
+let lat = "40";
+let lon = "50";
+let queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${openWeatherApiKey}`;
 
 fetch(queryURL)
 	.then((response) => {
 		return response.json();
 	})
-	.then((data) => console.log(data));
+	.then((data) => {
+		const location = data.city.name;
+		const forecast = data.list;
+		let dailyForecast = [];
+		for (i = 0; i < forecast.length; i = i + 8) {
+			dailyForecast.push(forecast[i]);
+		}
+		console.log(dailyForecast);
+	});

6dbd0e9c803a3db01a581a747ecff6771031ff5e
Blake Skor <blake.skor@outlook.com>
2023-11-09 03:10:21 -0500

    generate cards with jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,4 +16,31 @@ fetch(queryURL)
 			dailyForecast.push(forecast[i]);
 		}
 		console.log(dailyForecast);
+
+		const dailyDiv = $("#daily-wrapper");
+		dailyForecast.forEach((day) => {
+			const card = $('<div class="card" style="width: 15rem"></div>');
+			const header = $('<div class="card-header"></div>');
+			card.append(header);
+			const title = $('<h5 class="card-title"></h5>');
+			title.text("Date");
+			header.append(title);
+			const subtitle = $(
+				'<h6 class="card-subtitle mb-2 text-body-secondary">Emoji</h6>'
+			);
+			subtitle.text("icon");
+			header.append(subtitle);
+			const list = $('<ul class="list-group list-group-flush"></ul>');
+			card.append(list);
+			const temp = $('<li class="list-group-item"></li>');
+			temp.text("Temperature:");
+			list.append(temp);
+			const wind = $('<li class="list-group-item"></li>');
+			wind.text("Wind:");
+			list.append(wind);
+			const humidity = $('<li class="list-group-item"></li>');
+			humidity.text("Humidity:");
+			list.append(humidity);
+			dailyDiv.append(card);
+		});
 	});

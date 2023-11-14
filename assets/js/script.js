13ed8abbae49d7dc10be467e2ab99348fa2fbe96
Blake Skor <blake.skor@outlook.com>
2023-11-14 15:33:21 -0500

    use loc query as input for forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,67 +12,64 @@ fetch(queryLocURL)
 	})
 	.then((data) => {
 		const {lat, lon} = data[0];
-		console.log(lat);
-		console.log(lon);
-	});
+		const queryForecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${openWeatherApiKey}`;
+		const queryTodayURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${openWeatherApiKey}`;
 
-const lat = "40";
-const lon = "40";
-const queryForecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${openWeatherApiKey}`;
-const queryTodayURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${openWeatherApiKey}`;
+		fetch(queryForecastURL)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				const location = data.city.name;
+				const forecast = data.list;
+				console.log(forecast);
+				let dailyForecast = [];
+				for (i = 0; i < forecast.length; i = i + 8) {
+					dailyForecast.push(forecast[i]);
+				}
 
-fetch(queryForecastURL)
-	.then((response) => {
-		return response.json();
-	})
-	.then((data) => {
-		const location = data.city.name;
-		const forecast = data.list;
-		console.log(forecast);
-		let dailyForecast = [];
-		for (i = 0; i < forecast.length; i = i + 8) {
-			dailyForecast.push(forecast[i]);
-		}
+				const dailyDiv = $("#daily-wrapper");
+				dailyForecast.forEach((day) => {
+					const card = $('<div class="card" style="width: 15rem"></div>');
+					const header = $('<div class="card-header"></div>');
+					card.append(header);
+					const title = $('<h5 class="card-title"></h5>');
+					const date = dayjs(day.dt_txt);
+					title.text(date.format("M/DD/YYYY"));
+					header.append(title);
+					const iconUrl = getIcon(day.weather[0].icon);
+					const subtitle = $(
+						`<img class="bg-primary rounded-5" src=${iconUrl} />`
+					);
+					subtitle.text("icon");
+					header.append(subtitle);
+					const list = $('<ul class="list-group list-group-flush"></ul>');
+					card.append(list);
+					const temp = $('<li class="list-group-item"></li>');
+					temp.text(`Temperature: ${day.main.temp} °F`);
+					list.append(temp);
+					const wind = $('<li class="list-group-item"></li>');
+					wind.text(`Wind: ${day.wind.speed} MPH`);
+					list.append(wind);
+					const humidity = $('<li class="list-group-item"></li>');
+					humidity.text(`Humidity: ${day.main.humidity} %`);
+					list.append(humidity);
+					dailyDiv.append(card);
+				});
+			});
 
-		const dailyDiv = $("#daily-wrapper");
-		dailyForecast.forEach((day) => {
-			const card = $('<div class="card" style="width: 15rem"></div>');
-			const header = $('<div class="card-header"></div>');
-			card.append(header);
-			const title = $('<h5 class="card-title"></h5>');
-			const date = dayjs(day.dt_txt);
-			title.text(date.format("M/DD/YYYY"));
-			header.append(title);
-			const iconUrl = getIcon(day.weather[0].icon);
-			const subtitle = $(`<img class="bg-primary rounded-5" src=${iconUrl} />`);
-			subtitle.text("icon");
-			header.append(subtitle);
-			const list = $('<ul class="list-group list-group-flush"></ul>');
-			card.append(list);
-			const temp = $('<li class="list-group-item"></li>');
-			temp.text(`Temperature: ${day.main.temp} °F`);
-			list.append(temp);
-			const wind = $('<li class="list-group-item"></li>');
-			wind.text(`Wind: ${day.wind.speed} MPH`);
-			list.append(wind);
-			const humidity = $('<li class="list-group-item"></li>');
-			humidity.text(`Humidity: ${day.main.humidity} %`);
-			list.append(humidity);
-			dailyDiv.append(card);
-		});
-	});
-
-fetch(queryTodayURL)
-	.then((response) => {
-		return response.json();
-	})
-	.then((data) => {
-		console.log(data);
-		$("#current-location").text(
-			`${data.name} (${dayjs().format("M/DD/YYYY")})`
-		);
-		$("#current-temp").text(`Temperature: ${data.main.temp} °F`);
-		$("#current-icon").attr("src", getIcon(data.weather[0].icon));
-		$("#current-wind").text(`Wind: ${data.wind.speed} MPH`);
-		$("#current-humidity").text(`Humidity: ${data.main.humidity} %`);
+		fetch(queryTodayURL)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				$("#current-location").text(
+					`${data.name} (${dayjs().format("M/DD/YYYY")})`
+				);
+				$("#current-temp").text(`Temperature: ${data.main.temp} °F`);
+				$("#current-icon").attr("src", getIcon(data.weather[0].icon));
+				$("#current-wind").text(`Wind: ${data.wind.speed} MPH`);
+				$("#current-humidity").text(`Humidity: ${data.main.humidity} %`);
+			});
 	});
